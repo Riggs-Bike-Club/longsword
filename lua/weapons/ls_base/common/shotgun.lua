21d4f8fcde638640bbbf3b5d3d4fdd43f1edca25
Riggs <riggs9162@gmx.de>
2026-05-23 21:52:45 +0200

    refactor(shotgun): extract reload detection and normalize formatting
    Introduces a new method to centralize shotgun reload detection logic and applies consistent indentation and formatting throughout shotgun reload functions for improved maintainability and code readability.

--- lua/weapons/ls_base/common/shotgun.lua
+++ lua/weapons/ls_base/common/shotgun.lua
@@ -1,62 +1,68 @@
+function SWEP:UsesShotgunReload()
+    return self.Shotgun == true or self.LongswordMode == "shotgun"
+end
+
 function SWEP:ReloadShotgun()
-	if self.CanChamberShotgun and self:Clip1() == 0 then
-		self:PlayAnim( ACT_VM_RELOAD_EMPTY )
-		self:SetClip1( self:Clip1() + 1 )
-		self:GetOwner():RemoveAmmo( 1, self:GetPrimaryAmmoType() )
-	else
-		self:PlayAnim( ACT_SHOTGUN_RELOAD_START )
-	end
+    if self.CanChamberShotgun and self:Clip1() == 0 then
+        self:PlayAnim( ACT_VM_RELOAD_EMPTY )
+        self:SetClip1( self:Clip1() + 1 )
+        self:GetOwner():RemoveAmmo( 1, self:GetPrimaryAmmoType() )
+    else
+        self:PlayAnim( ACT_SHOTGUN_RELOAD_START )
+    end
 
-	self:GetOwner():DoReloadEvent()
-	self:QueueIdle()
+    self:GetOwner():DoReloadEvent()
+    self:QueueIdle()
 
-	self:SetReloading( true )
-	self:SetReloadTime( CurTime() + self:GetOwner():GetViewModel():SequenceDuration() )
+    self:SetReloading( true )
+    self:SetReloadTime( CurTime() + self:GetOwner():GetViewModel():SequenceDuration() )
 
-	if self.ReloadSound then
-		self:EmitSound(self.ReloadSound)
-	end
+    if self.ReloadSound then
+        self:EmitSound( self.ReloadSound )
+    end
 
-	hook.Run("LongswordWeaponReload", self:GetOwner(), self)
+    hook.Run( "LongswordWeaponReload", self:GetOwner(), self )
 end
 
 function SWEP:InsertShell()
-	self:SetClip1( self:Clip1() + 1 )
-	self:GetOwner():RemoveAmmo( 1, self:GetPrimaryAmmoType() )
+    self:SetClip1( self:Clip1() + 1 )
+    self:GetOwner():RemoveAmmo( 1, self:GetPrimaryAmmoType() )
 
-	self:PlayAnim(ACT_VM_RELOAD)
-	self:QueueIdle()
+    self:PlayAnim( ACT_VM_RELOAD )
+    self:QueueIdle()
 
-	self:SetReloadTime(CurTime() + (self.ShellInsertDelay or self:GetOwner():GetViewModel():SequenceDuration()))
+    self:SetReloadTime( CurTime() + ( self.ShellInsertDelay or self:GetOwner():GetViewModel():SequenceDuration() ) )
 
-	if self.ReloadShellSound then
-		self:EmitSound(self.ReloadShellSound)
-	end
+    if self.ReloadShellSound then
+        self:EmitSound( self.ReloadShellSound )
+    end
 end
 
 function SWEP:ShotgunReloadThink()
-	if self:GetReloadTime() > CurTime() then return end
+    if self:GetReloadTime() > CurTime() then return end
 
-	local cs = self.Primary.ClipSize
+    local clipSize = self.Primary.ClipSize
 
-	if self.CanChamberShotgun then
-		cs = cs + 1
-	end
+    if self.CanChamberShotgun then
+        clipSize = clipSize + 1
+    end
 
-	if self:Clip1() < cs and self:GetOwner():GetAmmoCount( self:GetPrimaryAmmoType() ) > 0
-		and not self:GetOwner():KeyDown( IN_ATTACK ) then
-		self:InsertShell()
-	else
-		self:FinishShotgunReload()
-	end
+    if self:Clip1() < clipSize and self:GetOwner():GetAmmoCount( self:GetPrimaryAmmoType() ) > 0
+        and !self:GetOwner():KeyDown( IN_ATTACK ) then
+        self:InsertShell()
+    else
+        self:FinishShotgunReload()
+    end
 end
 
 function SWEP:FinishShotgunReload()
-	self:SetReloading( false )
+    self:SetReloading( false )
 
-	self:PlayAnim( ACT_SHOTGUN_RELOAD_FINISH )
-	self:SetReloadTime( CurTime() + self:GetOwner():GetViewModel():SequenceDuration() )
-	self:QueueIdle()
+    self:PlayAnim( ACT_SHOTGUN_RELOAD_FINISH )
+    self:SetReloadTime( CurTime() + self:GetOwner():GetViewModel():SequenceDuration() )
+    self:QueueIdle()
 
-	if self.PumpSound then self:EmitSound( self.PumpSound ) end
+    if self.PumpSound then
+        self:EmitSound( self.PumpSound )
+    end
 end
